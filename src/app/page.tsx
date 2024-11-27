c7c8ddbeeb6017b6a2639565f3c780ee5b9ff050
Claire Wang <claire_wang123@yahoo.com>
2024-11-27 17:02:23 -0500

    adding plausible provider to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import Connections from './components/connections';
+import PlausibleProvider from 'next-plausible'
 import './App.scss';
 
 const data = {
@@ -43,36 +44,38 @@ function App() {
     }
 
     return (
-        <div className="App">
-            <div className="header">
-                <Image src="/logo.png" width={100} height={60} alt="Payne Mitchell Ramsey Law Group" />
-                {started ?
-                    <Image src="/help.png" width={40} height={54} alt="Help" id="gift" onClick={() => setPopup(2)} />
-                    :
-                    <></>
-                }
-            </div>
-            <div className="game-wrapper">
-                {started ?
-                    <Connections gameData={data} popup={popup} setPopup={setPopup} />
-                    :
-                    <div className="start-screen">
-                        <div className="start-text">
-                            <Image src="/gift.png" width={80} height={107} alt="gift" />
-                            <h1>Holiday Connections</h1>
-                            <p>Group words that share a common thread.</p>
-                            <p>Can you spot the connections?</p>
-                            <button onClick={transition}>PLAY</button>
+        <PlausibleProvider domain="pmr-connections.vercel.app/">
+            <div className="App">
+                <div className="header">
+                    <Image src="/logo.png" width={100} height={60} alt="Payne Mitchell Ramsey Law Group" />
+                    {started ?
+                        <Image src="/help.png" width={40} height={54} alt="Help" id="gift" onClick={() => setPopup(2)} />
+                        :
+                        <></>
+                    }
+                </div>
+                <div className="game-wrapper">
+                    {started ?
+                        <Connections gameData={data} popup={popup} setPopup={setPopup} />
+                        :
+                        <div className="start-screen">
+                            <div className="start-text">
+                                <Image src="/gift.png" width={80} height={107} alt="gift" />
+                                <h1>Holiday Connections</h1>
+                                <p>Group words that share a common thread.</p>
+                                <p>Can you spot the connections?</p>
+                                <button onClick={transition}>PLAY</button>
+                            </div>
+                            <div className="info-wrapper">
+                                <p>Each connection you make helps give back to the Legal Aid of NorthWest Texas, ensuring access to justice for individuals and communities in need.</p>
+                                <p>Want to donate directly to legal aid?</p>
+                                <a href="https://legalaidtx.org/support-our-work/donate/" target="_blank"><button className="donate-button">Donate Today</button></a>
+                            </div>
                         </div>
-                        <div className="info-wrapper">
-                            <p>Each connection you make helps give back to the Legal Aid of NorthWest Texas, ensuring access to justice for individuals and communities in need.</p>
-                            <p>Want to donate directly to legal aid?</p>
-                            <a href="https://legalaidtx.org/support-our-work/donate/" target="_blank"><button className="donate-button">Donate Today</button></a>
-                        </div>
-                    </div>
-                }
+                    }
+                </div>
             </div>
-        </div>
+        </PlausibleProvider>
     );
 }
 
